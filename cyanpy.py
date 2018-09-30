ecb5074a93e0c2f627accae7ee13600f41f04ae4
Emily Warr <32601476+EmilyWarr@users.noreply.github.com>
2018-10-01 00:46:44 +0100

    Uploaded cyanpy.py

---
+++ cyanpy.py
@@ -0,0 +1,14 @@
+#cyanpy - a collection of small but useful functions that I need quite often.
+
+#This functions save a dictionary to a file. 
+def dictToFile(aDict, fileName):
+    file = open(fileName, 'w')
+    file.write(str(aDict))
+    file.close()
+
+#This functions loads a dictions from a file.
+def fileToDict(fileName):
+    file = open(fileName, 'r')
+    aDict = eval(file.read())
+    file.close
+    return aDict
