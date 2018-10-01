24bba0fbab12984d69a1b961fad7202d0833872a
Emily Warr <32601476+EmilyWarr@users.noreply.github.com>
2018-10-01 15:47:18 +0100

    Add files via upload

--- cyanpy.py
+++ cyanpy.py
@@ -12,3 +12,21 @@ def fileToDict(fileName):
     aDict = eval(file.read())
     file.close
     return aDict
+
+#This functions saves the list to a file.
+def listToFile(aList, fileName):
+    print(str(len(aList)))
+    file = open(fileName, 'w')
+    for x in range(0, len(aList)):
+        if x == int(len(aList)-1):
+            file.write(aList[x])
+        else:
+            file.write(aList[x] + str('\n'))
+    file.close
+            
+#This functions loads a list from a file.
+def fileToList(fileName):
+    file = open(fileName,'r')
+    aList = file.readlines()
+    file.close
+    return aList
